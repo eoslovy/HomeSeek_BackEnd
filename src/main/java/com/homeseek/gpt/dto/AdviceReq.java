cf56f26b8a15e3a4a663645fdf311e6f9ee0f86b
윤덕건 <ydg0630@naver.com>
2024-11-21 17:35:30 +0900

    feat: add prompt

---
+++ src/main/java/com/homeseek/gpt/dto/AdviceReq.java
@@ -0,0 +1,12 @@
+package com.homeseek.gpt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdviceReq {
+    private String aptName;
+    private String si;
+    private String gu;
+}
