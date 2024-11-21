db3935425a5cf059572ddbe9abf4a5cde119d205
윤덕건 <ydg0630@naver.com>
2024-11-21 17:35:41 +0900

    feat: add trend

---
+++ src/main/java/com/homeseek/trend/dto/DistrictReq.java
@@ -0,0 +1,12 @@
+package com.homeseek.trend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class DistrictReq {
+    private List<String> districts;
+}
