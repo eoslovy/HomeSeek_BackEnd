4ae8510cb5783fc54d5e7869ecc1ab1ed7fcd6f2
윤덕건 <ydg0630@naver.com>
2024-11-21 17:34:34 +0900

    feat: add prompt

--- src/main/java/com/homeseek/gpt/service/GPTService.java
+++ src/main/java/com/homeseek/gpt/service/GPTService.java
@@ -2,5 +2,6 @@ package com.homeseek.gpt.service;
 
 public interface GPTService {
     String getChatCompletion(String userMessage);
-    String getAdvice(String userMessage);
+//    String getAdvice(String userMessage);
+    String getAdvice(String aptName, String si, String gu);
 }
