013c680eb90b7b6067e5114ba4566716a6783c24
윤덕건 <ydg0630@naver.com>
2024-11-21 17:34:44 +0900

    feat: add trend

--- src/main/java/com/homeseek/trend/service/TrendService.java
+++ src/main/java/com/homeseek/trend/service/TrendService.java
@@ -1,8 +1,10 @@
 package com.homeseek.trend.service;
 
+import com.homeseek.trend.dto.DistrictReq;
 import com.homeseek.trend.dto.TrendReq;
 import com.homeseek.trend.dto.TrendResp;
 
 public interface TrendService {
     TrendResp getTrendData(TrendReq trendReq);
+    TrendResp getDistrictTrendData(DistrictReq request);
 }
